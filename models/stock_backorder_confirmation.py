6cf8c6ec702e0437032b5dbfd6db6ac0a4833a50
hoang <hoangdd@magenest.com>
2022-08-23 13:17:18 +0700

    fix-delivery

---
+++ models/stock_backorder_confirmation.py
@@ -0,0 +1,44 @@
+
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_compare
+
+
+class StockBackorderConfirmation(models.TransientModel):
+    _inherit = 'stock.backorder.confirmation'
+    _description = 'Backorder Confirmation'
+
+    def process(self):
+        pickings_to_do = self.env['stock.picking']
+        pickings_not_to_do = self.env['stock.picking']
+        for line in self.backorder_confirmation_line_ids:
+            if line.to_backorder is True:
+                pickings_to_do |= line.picking_id
+            else:
+                pickings_not_to_do |= line.picking_id
+
+        for pick_id in pickings_not_to_do:
+            moves_to_log = {}
+            for move in pick_id.move_lines:
+                if float_compare(move.product_uom_qty,
+                                 move.quantity_done,
+                                 precision_rounding=move.product_uom.rounding) > 0:
+                    moves_to_log[move] = (move.quantity_done, move.product_uom_qty)
+            pick_id._log_less_quantities_than_expected(moves_to_log)
+
+        pickings_to_validate = self.env.context.get('button_validate_picking_ids')
+        if pickings_to_validate:
+            pickings_to_validate = self.env['stock.picking'].browse(pickings_to_validate).with_context(skip_backorder=True)
+            if pickings_not_to_do:
+                pickings_to_validate = pickings_to_validate.with_context \
+                    (picking_ids_not_to_backorder=pickings_not_to_do.ids)
+
+            pickings_to_validate.button_validate()
+            action = self.env["ir.actions.actions"]._for_xml_id("stock.action_picking_tree_all")
+            action['domain'] = [('sale_id', '=', self.pick_ids.sale_id.id)]
+            return action
+
+        return True
